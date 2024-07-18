08fe11f3961b96e104ccf7371cc5ec85ad8af3b6
Jeon Seung Hwan <fabronjeon@naver.com>
2024-07-18 16:48:25 +0900

    메뉴 드롭다운 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,17 @@
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import "../assets/styles/Header.css";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 function Header() {
-    // const scrollToSection = (sectionId) => {
-    //     document.getElementById(sectionId).scrollIntoView({
-    //         behavior: "smooth",
-    //     });
-    // };
-    const scrollToTop = () => {
+    function scrollToTop() {
         window.scrollTo({
             top: 0,
             behavior: "smooth",
         });
-    };
-    const scrollToSection = (sectionId) => {
+    }
+
+    function scrollToSection(sectionId) {
         const element = document.getElementById(sectionId);
         if (element) {
             const headerOffset = document.querySelector(".header").offsetHeight;
@@ -28,7 +26,8 @@ function Header() {
         } else {
             console.error(`Element with id ${sectionId} not found.`);
         }
-    };
+    }
+
     return (
         <div className="header">
             <div onClick={scrollToTop} className="nav-logo"></div>
@@ -37,16 +36,22 @@ function Header() {
                     매장 소개
                 </li>
                 <li
-                    className="nav-item"
+                    className="nav-item nav-menu"
                     onClick={() => scrollToSection("representative-section")}
                 >
                     메뉴
+                    <ul className="dropdown-content">
+                        <li>대표 메뉴</li>
+                        <li>베이커리</li>
+                        <li>커피/음료</li>
+                        <li>디저트</li>
+                    </ul>
                 </li>
                 <li
                     className="nav-item"
                     onClick={() => scrollToSection("direction-section")}
                 >
-                    오시는 길
+                    오시는 길<span></span>
                 </li>
                 <li
                     className="nav-item"
